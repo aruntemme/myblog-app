274eaac6910212e4f5e89003ee5967026f5d3956
aruntemme <arungauthamk@gmail.com>
2019-11-26 07:14:01 +0530

    type: title icon,name changed

--- blog/settings.py
+++ blog/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=$e6938mdlml*_#xtk415bmuw48hbx55$!jx88+4!h#_)oq9*t'
+SECRET_KEY = os.environ.get('BLOG_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
